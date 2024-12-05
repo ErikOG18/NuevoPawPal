05f39f6db1e4d7a0ec2a6dfc1313dd0a03324d02
ErickOG <rarazielortizra@gmail.com>
2024-12-05 12:44:20 -0600

    first commit

---
+++ src/main/resources/static/agendarServicioScripts.js
@@ -0,0 +1,47 @@
+const registrar = document.getElementById('botonregistrarcita');
+
+registrar.addEventListener('click', () => {
+    const clientName = document.getElementById('clientName').value;
+    const petName = document.getElementById('petName').value;
+    const breed = document.getElementById('breed').value;
+    const email = document.getElementById('email').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const dateStr = document.getElementById('date').value;
+    const kindOfService = document.getElementById('kindOfService').value;
+    const comment = document.getElementById('comment').value;
+    const privacyPolicyAccepted = document.getElementById('privacyPolicyAccepted').checked;
+
+    const newDate = new Date(dateStr);
+    const formattedDate = newDate.toISOString().slice(0, 16);
+    // Crear objeto para enviar al backend
+    const service = {
+        clientName: clientName,
+        petName: petName,
+        breed: breed,
+        email: email,
+        phoneNumber: phoneNumber,
+        date: formattedDate,
+        kindOfService: kindOfService,
+        comment: comment,
+        privacyPolicyAccepted: privacyPolicyAccepted
+    };
+
+    console.log(formattedDate);
+    // Realizar la llamada a la API
+    const url = `http://35.171.26.213:8080/api/v5/post-service`;
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(service)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Guardado', data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
---
+++ src/main/resources/static/newsletterScript.js
@@ -0,0 +1,28 @@
+const suscribir = document.getElementById('button-addon2');
+
+suscribir.addEventListener('click', () => {
+    const email = document.getElementById('emailInputN').value;
+
+    // Crear objeto para enviar al backend
+    const newsletter = {
+        email: email,
+    };
+console.log(email);
+    // Realizar la llamada a la API
+    const url = `http://35.171.26.213:8080/api/v6/post-newsletter`;
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newsletter)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Guardado', data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
